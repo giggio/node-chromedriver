d1ec1f2752aecbc1bcec36cb713c0c2c1f2cf2f6
Giovanni Bassi <giggio@giggio.net>
2016-08-17 16:08:25 -0300

    Fail installation for Mac 32 bits

--- install.js
+++ install.js
@@ -28,7 +28,12 @@ if (platform === 'linux') {
     platform += '32'
   }
 } else if (platform === 'darwin') {
-  platform = 'mac64'
+  if (process.arch === 'x64') {
+    platform = 'mac64'
+  } else {
+    console.log('Only Mac 64 bits supported.');
+    process.exit(1);
+  }
 } else if (platform !== 'win32') {
   console.log('Unexpected platform or architecture:', process.platform, process.arch)
   process.exit(1)
