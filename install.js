deb498c959e02b7fe1ed8b2732ae0fa198d44cb9
TokinRing <techsupport@altservice.com>
2016-11-19 12:14:29 -0800

    Added support for FreeBSD to install.js

--- install.js
+++ install.js
@@ -34,6 +34,12 @@ if (platform === 'linux') {
     console.log('Only Mac 64 bits supported.');
     process.exit(1);
   }
+} else if (platform === 'freebsd') {
+  if (process.arch === 'x64') {
+    platform = 'mac64'
+  } else {
+    platform = 'mac32'
+  }
 } else if (platform !== 'win32') {
   console.log('Unexpected platform or architecture:', process.platform, process.arch)
   process.exit(1)
