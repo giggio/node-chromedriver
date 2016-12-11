c1b3cf797a5d90e73e0250424d73cc02f753b3a7
Giovanni Bassi <giggio@giggio.net>
2016-12-11 21:49:24 -0200

    Add eslint and fix issues
    Signed-off-by: Giovanni Bassi <giggio@giggio.net>

--- install.js
+++ install.js
@@ -1,105 +1,104 @@
-'use strict'
+'use strict';
 
-var AdmZip = require('adm-zip')
-var cp = require('child_process')
-var fs = require('fs')
-var helper = require('./lib/chromedriver')
-var http = require('http')
-var https = require('https')
-var kew = require('kew')
-var npmconf = require('npmconf')
-var mkdirp = require('mkdirp')
-var path = require('path')
-var rimraf = require('rimraf').sync
-var url = require('url')
-var util = require('util')
+var AdmZip = require('adm-zip');
+var fs = require('fs');
+var helper = require('./lib/chromedriver');
+var http = require('http');
+var https = require('https');
+var kew = require('kew');
+var npmconf = require('npmconf');
+var mkdirp = require('mkdirp');
+var path = require('path');
+var rimraf = require('rimraf').sync;
+var url = require('url');
+var util = require('util');
 
-var libPath = path.join(__dirname, 'lib', 'chromedriver')
-var cdnUrl = process.env.npm_config_chromedriver_cdnurl || process.env.CHROMEDRIVER_CDNURL || 'https://chromedriver.storage.googleapis.com'
+var libPath = path.join(__dirname, 'lib', 'chromedriver');
+var cdnUrl = process.env.npm_config_chromedriver_cdnurl || process.env.CHROMEDRIVER_CDNURL || 'https://chromedriver.storage.googleapis.com';
 // adapt http://chromedriver.storage.googleapis.com/
-cdnUrl = cdnUrl.replace(/\/+$/, '')
-var downloadUrl = cdnUrl + '/%s/chromedriver_%s.zip'
-var platform = process.platform
+cdnUrl = cdnUrl.replace(/\/+$/, '');
+var downloadUrl = cdnUrl + '/%s/chromedriver_%s.zip';
+var platform = process.platform;
 
 if (platform === 'linux') {
   if (process.arch === 'x64') {
-    platform += '64'
+    platform += '64';
   } else {
-    platform += '32'
+    platform += '32';
   }
 } else if (platform === 'darwin') {
   if (process.arch === 'x64') {
-    platform = 'mac64'
+    platform = 'mac64';
   } else {
     console.log('Only Mac 64 bits supported.');
     process.exit(1);
   }
 } else if (platform !== 'win32') {
-  console.log('Unexpected platform or architecture:', process.platform, process.arch)
-  process.exit(1)
+  console.log('Unexpected platform or architecture:', process.platform, process.arch);
+  process.exit(1);
 }
 
 downloadUrl = util.format(downloadUrl, helper.version, platform);
 
-var fileName = downloadUrl.split('/').pop()
+var fileName = downloadUrl.split('/').pop();
 
 npmconf.load(function(err, conf) {
   if (err) {
-    console.log('Error loading npm config')
-    console.error(err)
-    process.exit(1)
-    return
+    console.log('Error loading npm config');
+    console.error(err);
+    process.exit(1);
+    return;
   }
 
-  var tmpPath = findSuitableTempDirectory(conf)
-  var downloadedFile = path.join(tmpPath, fileName)
-  var promise = kew.resolve(true)
+  var tmpPath = findSuitableTempDirectory(conf);
+  var downloadedFile = path.join(tmpPath, fileName);
+  var promise = kew.resolve(true);
 
   // Start the install.
   promise = promise.then(function () {
-    console.log('Downloading', downloadUrl)
-    console.log('Saving to', downloadedFile)
-    return requestBinary(getRequestOptions(conf), downloadedFile)
-  })
+    console.log('Downloading', downloadUrl);
+    console.log('Saving to', downloadedFile);
+    return requestBinary(getRequestOptions(conf), downloadedFile);
+  });
 
   promise.then(function () {
-    return extractDownload(downloadedFile, tmpPath)
+    return extractDownload(downloadedFile, tmpPath);
   })
   .then(function () {
-    return copyIntoPlace(tmpPath, libPath)
+    return copyIntoPlace(tmpPath, libPath);
   })
   .then(function () {
-    return fixFilePermissions()
+    return fixFilePermissions();
   })
   .then(function () {
-    console.log('Done. ChromeDriver binary available at', helper.path)
+    console.log('Done. ChromeDriver binary available at', helper.path);
   })
   .fail(function (err) {
-    console.error('ChromeDriver installation failed', err)
-    process.exit(1)
-  })
-})
+    console.error('ChromeDriver installation failed', err);
+    process.exit(1);
+  });
+});
 
 
 function findSuitableTempDirectory(npmConf) {
-  var now = Date.now()
+  var now = Date.now();
   var candidateTmpDirs = [
     process.env.TMPDIR || npmConf.get('tmp'),
     '/tmp',
     path.join(process.cwd(), 'tmp')
-  ]
+  ];
 
   for (var i = 0; i < candidateTmpDirs.length; i++) {
-    var candidatePath = path.join(candidateTmpDirs[i], 'chromedriver')
+    var candidatePath = path.join(candidateTmpDirs[i], 'chromedriver');
 
     try {
-      mkdirp.sync(candidatePath, '0777')
-      var testFile = path.join(candidatePath, now + '.tmp')
-      fs.writeFileSync(testFile, 'test')
-      fs.unlinkSync(testFile)
-      return candidatePath
+      mkdirp.sync(candidatePath, '0777');
+      var testFile = path.join(candidatePath, now + '.tmp');
+      fs.writeFileSync(testFile, 'test');
+      fs.unlinkSync(testFile);
+      return candidatePath;
     } catch (e) {
-      console.log(candidatePath, 'is not writable:', e.message)
+      console.log(candidatePath, 'is not writable:', e.message);
     }
   }
 
@@ -112,102 +111,101 @@ function getRequestOptions(conf) {
   var options = url.parse(downloadUrl);
   var proxyUrl = options.protocol === 'https:' ? conf.get('https-proxy') : conf.get('proxy');
   if (proxyUrl) {
-    options = url.parse(proxyUrl)
-    options.path = downloadUrl
-    options.headers = { Host: url.parse(downloadUrl).host }
+    options = url.parse(proxyUrl);
+    options.path = downloadUrl;
+    options.headers = { Host: url.parse(downloadUrl).host };
     // Turn basic authorization into proxy-authorization.
     if (options.auth) {
-      options.headers['Proxy-Authorization'] = 'Basic ' + new Buffer(options.auth).toString('base64')
-      delete options.auth
+      options.headers['Proxy-Authorization'] = 'Basic ' + new Buffer(options.auth).toString('base64');
+      delete options.auth;
     }
   } else {
-    options = url.parse(downloadUrl)
+    options = url.parse(downloadUrl);
   }
 
-  options.rejectUnauthorized = !!process.env.npm_config_strict_ssl
+  options.rejectUnauthorized = !!process.env.npm_config_strict_ssl;
 
   // Use certificate authority settings from npm
-  var ca = process.env.npm_config_ca
+  var ca = process.env.npm_config_ca;
   if (!ca && process.env.npm_config_cafile) {
     try {
       ca = fs.readFileSync(process.env.npm_config_cafile, {encoding: 'utf8'})
-        .split(/\n(?=-----BEGIN CERTIFICATE-----)/g)
+        .split(/\n(?=-----BEGIN CERTIFICATE-----)/g);
 
       // Comments at the beginning of the file result in the first
       // item not containing a certificate - in this case the
       // download will fail
       if (ca.length > 0 && !/-----BEGIN CERTIFICATE-----/.test(ca[0])) {
-        ca.shift()
+        ca.shift();
       }
 
     } catch (e) {
-      console.error('Could not read cafile', process.env.npm_config_cafile, e)
+      console.error('Could not read cafile', process.env.npm_config_cafile, e);
     }
   }
 
   if (ca) {
-    console.log('Using npmconf ca')
+    console.log('Using npmconf ca');
     options.agentOptions = {
       ca: ca
-    }
-    options.ca = ca
+    };
+    options.ca = ca;
   }
 
-  return options
+  return options;
 }
 
 
 function requestBinary(requestOptions, filePath) {
-  var deferred = kew.defer()
+  var deferred = kew.defer();
 
-  var count = 0
-  var notifiedCount = 0
-  var outFile = fs.openSync(filePath, 'w')
+  var count = 0;
+  var notifiedCount = 0;
+  var outFile = fs.openSync(filePath, 'w');
 
-  var protocol = requestOptions.protocol === 'https:' ? https : http
+  var protocol = requestOptions.protocol === 'https:' ? https : http;
   var client = protocol.get(requestOptions, function (response) {
-    var status = response.statusCode
-    console.log('Receiving...')
+    var status = response.statusCode;
+    console.log('Receiving...');
 
     if (status === 200) {
       response.addListener('data',   function (data) {
-        fs.writeSync(outFile, data, 0, data.length, null)
-        count += data.length
+        fs.writeSync(outFile, data, 0, data.length, null);
+        count += data.length;
         if ((count - notifiedCount) > 800000) {
-          console.log('Received ' + Math.floor(count / 1024) + 'K...')
-          notifiedCount = count
+          console.log('Received ' + Math.floor(count / 1024) + 'K...');
+          notifiedCount = count;
         }
-      })
+      });
 
       response.addListener('end',   function () {
-        console.log('Received ' + Math.floor(count / 1024) + 'K total.')
-        fs.closeSync(outFile)
-        deferred.resolve(true)
-      })
+        console.log('Received ' + Math.floor(count / 1024) + 'K total.');
+        fs.closeSync(outFile);
+        deferred.resolve(true);
+      });
 
     } else {
-      client.abort()
-      deferred.reject('Error with http request: ' + util.inspect(response.headers))
+      client.abort();
+      deferred.reject('Error with http request: ' + util.inspect(response.headers));
     }
-  })
+  });
 
-  return deferred.promise
+  return deferred.promise;
 }
 
 
 function extractDownload(filePath, tmpPath) {
-  var deferred = kew.defer()
-  var options = {cwd: tmpPath}
+  var deferred = kew.defer();
 
-  console.log('Extracting zip contents')
+  console.log('Extracting zip contents');
   try {
-    var zip = new AdmZip(filePath)
-    zip.extractAllTo(tmpPath, true)
-    deferred.resolve(true)
+    var zip = new AdmZip(filePath);
+    zip.extractAllTo(tmpPath, true);
+    deferred.resolve(true);
   } catch (err) {
-    deferred.reject('Error extracting archive ' + err.stack)
+    deferred.reject('Error extracting archive ' + err.stack);
   }
-  return deferred.promise
+  return deferred.promise;
 }
 
 
@@ -242,11 +240,11 @@ function copyIntoPlace(tmpPath, targetPath) {
 function fixFilePermissions() {
   // Check that the binary is user-executable and fix it if it isn't (problems with unzip library)
   if (process.platform != 'win32') {
-    var stat = fs.statSync(helper.path)
+    var stat = fs.statSync(helper.path);
     // 64 == 0100 (no octal literal in strict mode)
     if (!(stat.mode & 64)) {
-      console.log('Fixing file permissions')
-      fs.chmodSync(helper.path, '755')
+      console.log('Fixing file permissions');
+      fs.chmodSync(helper.path, '755');
     }
   }
 }
