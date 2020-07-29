d5a8f1d25817b106ac5519d53f8e2c16365f1f9c
Greg Willard (r3pwn) <wickett06@gmail.com>
2020-07-29 12:35:02 -0400

    [install] move force_download to install.js

--- install.js
+++ install.js
@@ -15,25 +15,28 @@ let cdnUrl = process.env.npm_config_chromedriver_cdnurl || process.env.CHROMEDRI
 cdnUrl = cdnUrl.replace(/\/+$/, '');
 let detect_chromedriver_version = process.env.npm_config_detect_chromedriver_version || process.env.DETECT_CHROMEDRIVER_VERSION;
 let chromedriver_version = process.env.npm_config_chromedriver_version || process.env.CHROMEDRIVER_VERSION || helper.version;
+let force_download = process.env.npm_config_chromedriver_force_download === 'true' || process.env.CHROMEDRIVER_FORCE_DOWNLOAD === 'true';
 
 (async function install() {
   let options = {
-    cdn_url: cdnUrl,
-    force_download: "true"
+    cdn_url: cdnUrl
   };
 
-  if (chromedriver_version === 'LATEST') {
-    chromedriver_version = await utils.getChromeDriverVersion(utils.getRequestOptions(`${cdnUrl}/LATEST_RELEASE`));
-  } else {
-    let latestReleaseForVersionMatch = chromedriver_version.match(/LATEST_(\d+)/);
-    if (latestReleaseForVersionMatch) {
-      let majorVersion = latestReleaseForVersionMatch[1];
-      chromedriver_version = await utils.getChromeDriverVersion(utils.getRequestOptions(`${cdnUrl}/LATEST_RELEASE_${majorVersion}`));
-    }
+  if (force_download) {
+    options.force_download = "true";
   }
 
   if (detect_chromedriver_version !== 'true') {
-    options.download_version = chromedriver_version
+    if (chromedriver_version === 'LATEST') {
+      chromedriver_version = await utils.getChromeDriverVersion(utils.getRequestOptions(`${cdnUrl}/LATEST_RELEASE`));
+    } else {
+      let latestReleaseForVersionMatch = chromedriver_version.match(/LATEST_(\d+)/);
+      if (latestReleaseForVersionMatch) {
+        let majorVersion = latestReleaseForVersionMatch[1];
+        chromedriver_version = await utils.getChromeDriverVersion(utils.getRequestOptions(`${cdnUrl}/LATEST_RELEASE_${majorVersion}`));
+      }
+    }
+    options.download_version = chromedriver_version;
   }
 
   await (helper.download(options))
