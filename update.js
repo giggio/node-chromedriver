4ea3506373fa52f6c553d348c84763066dd374da
Giovanni Bassi <giggio@giggio.net>
2023-07-24 20:12:51 -0300

    Update update.js to work with Chrome for testing

--- update.js
+++ update.js
@@ -5,12 +5,12 @@ const execSync = require('child_process').execSync;
 const CURRENT_VERSION = require('./lib/chromedriver').version;
 
 // fetch the latest chromedriver version
-async function getLatest () {
-  const url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE';
+async function getLatest() {
+  const url = 'https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json';
   try {
     const res = await fetch(url);
-    const text = await res.text();
-    return text.trim();
+    const data = await res.json();
+    return data?.channels?.Stable?.version;
   } catch (err) {
     console.log(err);
     process.exit(1);
@@ -23,7 +23,7 @@ async function getLatest () {
    - add a git tag using the new node-chromedriver version
    - add a git commit, e.g. Bump version to 77.0.0
 */
-const writeUpdate = (version) => {
+async function writeUpdate(version) {
   const helper = fs.readFileSync('./lib/chromedriver.js', 'utf8');
   const versionExport = 'exports.version';
   const regex = new RegExp(`^.*${versionExport}.*$`, 'gm');
@@ -31,15 +31,20 @@ const writeUpdate = (version) => {
   fs.writeFileSync('./lib/chromedriver.js', updated, 'utf8');
   const packageVersion = `${version.slice(0, version.indexOf('.'))}.0.0`;
   execSync(`npm version ${packageVersion} --git-tag-version=false && git add . && git commit -m "Bump version to ${packageVersion}" && git tag -s ${packageVersion} -m ${packageVersion}`);
-};
+}
 
-getLatest().then(version => {
-  if (CURRENT_VERSION === version) {
-    console.log('Chromedriver version is up to date.');
-  } else {
-    writeUpdate(version);
-    console.log(`Chromedriver version updated to ${version}`);
+async function run() {
+  try {
+    const version = await getLatest();
+    if (CURRENT_VERSION === version) {
+      console.log('Chromedriver version is up to date.');
+    } else {
+      writeUpdate(version);
+      console.log(`Chromedriver version updated to ${version}`);
+    }
+  } catch (err) {
+    console.log(err);
   }
-}, err => {
-  console.log(err);
-});
+}
+
+run();
